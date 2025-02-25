ad985b6f26475371e4a8c4fb41262e084b48303a
Thiago Santos <thiagorj7@hotmail.com>
2025-02-25 16:26:42 +0000

    ATUALIZAÇÃO DE ARQUIVOS

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h> // Inclui a biblioteca padrão de entrada e saída
 int main() { // Função principal
     // Declaração de variáveis da carta 1
-    int populacao1, populacao2; //Declara o numero de habitantes na cidade 1 e 2
+    unsigned long int populacao1, populacao2; //Declara o numero de habitantes na cidade 1 e 2
     int pontos1, pontos2; // Declara a quantidade de pontos turísticos na cidade 1 e 2
     float area1, area2; // Declara a área da cidade 1 e 2 em km²
     float pib1, pib2; // Declara o PIB (Produto Interno Bruto) da cidade 1 e 2
@@ -10,6 +10,7 @@ int main() { // Função principal
     char codigo1[2], codigo2[2]; // Declara o código numerico de 01 a 04 para a cidade 1 e 2
     float densidade1, densidade2; // Declara a densidade populacional da cidade 1 e 2
     float pibcapita1, pibcapita2; // Declara o PIB per capita da cidade 1 e 2
+    float superpoder1, superpoder2; // Declara o superpoder da cidade 1 e 2
 
 
     // LEITURA DA CARTA 1
@@ -30,7 +31,7 @@ int main() { // Função principal
     printf("Digite o nome da cidade da 1ª Carta (Nomes simples. ex.: SALVADOR): "); // Solicita o nome da cidade da carta 1
     scanf("%s", &nome1); // Lê o nome da cidade da carta 1
     printf("Digite a quatidade de habitantes da cidade da 1ª Carta (Número inteiro): "); // Solicita o número de habitantes da carta 1
-    scanf("%d", &populacao1); // Lê o número de habitantes da carta 1
+    scanf("%ld", &populacao1); // Lê o número de habitantes da carta 1
     printf("Digite o tamanho da área em km² da cidade da 1ª Carta (Número inteiro): "); // Solicita o tamanho da área da carta 1
     scanf("%f", &area1); // Lê o tamanho da área da carta 1
     printf("Digite o valor do PIB da cidade da 1ª Carta (Número inteiro): "); // Solicita o valor do PIB da carta 1
@@ -39,6 +40,7 @@ int main() { // Função principal
     scanf("%d", &pontos1); // Lê a quantidade de pontos turísticos da carta 1
     densidade1 = (float)(populacao1 / area1); // Calculo da densidade populacional da carta 1
     pibcapita1 = (float)(pib1 / populacao1); // Calculo do PIB per capita da carta 1
+    superpoder1 = (float)(area1 + pib1 + populacao1 + pibcapita1 + pontos1 - densidade1); // Calculo do superpoder da carta 1
 
     // IMPRESSÃO DA CARTA 1
 
@@ -50,12 +52,13 @@ int main() { // Função principal
     printf("Estado: %c\n", opcao1); //Campo para a informação da letra do Estado da carta 1
     printf("Código: %c%s\n", opcao1, codigo1); //Campo para a informação do Códico com a letra do Estado e o número da carta da carta 1
     printf("Nome da cidade: %s\n", nome1); //Campo para a informação do nome da cidade da carta 1 (Nome simple, ex.: FORTALEZA)
-    printf("População: %d\n", populacao1); //Campo para a informação do número da população da carta 1
+    printf("População: %ld\n", populacao1); //Campo para a informação do número da população da carta 1
     printf("Área: %.2f km²\n", area1); //Campo para a informação do tamanho da área da cidade  da carta 1 informada acima em Km²
     printf("PIB: %.2f reais\n", pib1); //Campo para a informação do PIB (Produto Interno Bruto) da carta 1
     printf("Numero de Pontos Turísticos: %d\n", pontos1); //Campo para a informação de quantos pontos turístico existem na cidade  da carta 1(Aproximadamente)
     printf("Densidade populacional: %.2f hab/km²\n", densidade1); //Campo para a informação da densidade populacional da cidade da carta 1
     printf("PIB per capita: %.2f reais\n", pibcapita1); //Campo para a informação do PIB per capita da cidade da carta 1
+    printf("Superpoder: %.2f\n", superpoder1); //Campo para a informação do superpoder da cidade da carta 1
     printf("\n"); // Pular linha
     printf("---------------------------------------------------\n"); // Colocando um tracejado entre as informações para separar
     printf("\n"); // Pular linha
@@ -79,7 +82,7 @@ int main() { // Função principal
     printf("Digite o nome da cidade da 2ª Carta (Nomes simples. ex.: SALVADOR): "); // Solicita o nome da cidade da carta 2
     scanf("%s", &nome2); // Lê o nome da cidade da carta 2
     printf("Digite a quatidade de habitantes da cidade da 2ª Carta (Número inteiro): "); // Solicita o número de habitantes da carta 2
-    scanf("%d", &populacao2); // Lê o número de habitantes da carta 2
+    scanf("%ld", &populacao2); // Lê o número de habitantes da carta 2
     printf("Digite o tamanho da área em km² da cidade da 2ª Carta (Número inteiro): "); // Solicita o tamanho da área da carta 2
     scanf("%f", &area2); // Lê o tamanho da área da carta 2
     printf("Digite o valor do PIB da cidade da 2ª Carta (Número inteiro): "); // Solicita o valor do PIB da carta 2
@@ -88,6 +91,7 @@ int main() { // Função principal
     scanf("%d", &pontos2); // Lê a quantidade de pontos turísticos da carta 2
     densidade2 = (float)(populacao2 / area2); // Calculo da densidade populacional da carta 2
     pibcapita2 = (float)(pib2 / populacao2); // Calculo do PIB per capita da carta 2
+    superpoder2 = (float)(area2 + pib2 + populacao2 + pibcapita2 + pontos2 - densidade2); // Calculo do superpoder da carta 2
 
 
     // IMPRESSÃO DA CARTA 2
@@ -100,12 +104,13 @@ int main() { // Função principal
     printf("Estado: %c\n", opcao2); //Campo para a informação da letra do Estado
     printf("Código: %c%s\n", opcao2, codigo2); //Campo para a informação do Códico com a letra do Estado e o número da carta
     printf("Nome da cidade: %s\n", nome2); //Campo para a informação do nome da cidade (Nome simple, ex.: FORTALEZA)
-    printf("População: %d\n", populacao2); //Campo para a informação do número da população
+    printf("População: %ld\n", populacao2); //Campo para a informação do número da população
     printf("Área: %.2f km²\n", area2); //Campo para a informação do tamanho da área da cidade informada acima em Km²
     printf("PIB: %.2f reais\n", pib2); //Campo para a informação do PIB (Produto Interno Bruto)
     printf("Numero de Pontos Turísticos: %d\n", pontos2); //Campo para a informação de quantos pontos turístico existem na cidade (Aproximadamente)
     printf("Densidade populacional: %.2f hab/km²\n", densidade2); //Campo para a informação da densidade populacional da cidade da carta 2
     printf("PIB per capita: %.2f reais\n", pibcapita2); //Campo para a informação do PIB per capita da cidade da carta 2
+    printf("Superpoder: %.2f\n", superpoder2); //Campo para a informação do superpoder da cidade da carta 2
     printf("\n"); // Pular linha
     printf("---------------------------------------------------\n"); // Colocando um tracejado entre as informações para separar
     printf("\n"); // Pular linha
@@ -113,6 +118,13 @@ int main() { // Função principal
     printf("SUAS INFORMAÇÕES FORAM CADASTRADAS COM SUCESSO!!!\n"); // Mensagem informativa
     printf("OBRIGADO POR PARTICIPAR!!!\n"); // Mensagem informativa
     printf("\n"); // Pular linha
+    printf("SEGUEM AS ABAIXO COMPARAÇÕES DAS CARTAS CADASTRADAS:\n"); // Mensagem informativa
+    printf("\n"); // Pular linha
+    printf("---------------------------------------------------\n"); // Colocando um tracejado entre as informações para separar
+    printf("\n"); // Pular linha
+
+    // COMPARAÇÃO DAS CARTAS
+
 
     return 0; // Retorna 0 para o sistema operacional
 
