d49d9d98377c2040d5b7b3716b8c5bba0621a72a
Thiago Santos <thiagorj7@hotmail.com>
2025-08-19 13:42:56 +0000

    executar o comando 'git commit' para registrar as alterações no repositório.

---
+++ thaila.c
@@ -0,0 +1,61 @@
+
+Microsoft Visual Studio Solution File, Format Version 11.00
+# Visual Studio 2010
+Project("{F184B08F-C81C-45F6-A57F-5ABD9991F28F}") = "WindowsApplication3", "WindowsApplication3\WindowsApplication3.vbproj", "{C326D55B-0411-46F5-B9A6-7C7DFF26940B}"
+EndProject
+Project("{E24C65DC-7377-472B-9ABA-BC803B73C61A}") = "WebSite1", "..\..\WebSites\WebSite1\", "{3B5FBBEF-CC0C-41C9-AD5F-8037EBCC9A92}"
+	ProjectSection(WebsiteProperties) = preProject
+		TargetFrameworkMoniker = ".NETFramework,Version%3Dv4.0"
+		Debug.AspNetCompiler.VirtualPath = "/WebSite1"
+		Debug.AspNetCompiler.PhysicalPath = "..\..\WebSites\WebSite1\"
+		Debug.AspNetCompiler.TargetPath = "PrecompiledWeb\WebSite1\"
+		Debug.AspNetCompiler.Updateable = "true"
+		Debug.AspNetCompiler.ForceOverwrite = "true"
+		Debug.AspNetCompiler.FixedNames = "false"
+		Debug.AspNetCompiler.Debug = "True"
+		Release.AspNetCompiler.VirtualPath = "/WebSite1"
+		Release.AspNetCompiler.PhysicalPath = "..\..\WebSites\WebSite1\"
+		Release.AspNetCompiler.TargetPath = "PrecompiledWeb\WebSite1\"
+		Release.AspNetCompiler.Updateable = "true"
+		Release.AspNetCompiler.ForceOverwrite = "true"
+		Release.AspNetCompiler.FixedNames = "false"
+		Release.AspNetCompiler.Debug = "False"
+		VWDPort = "20437"
+		DefaultWebSiteLanguage = "Visual Basic"
+	EndProjectSection
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Debug|Mixed Platforms = Debug|Mixed Platforms
+		Debug|x86 = Debug|x86
+		Release|Any CPU = Release|Any CPU
+		Release|Mixed Platforms = Release|Mixed Platforms
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{C326D55B-0411-46F5-B9A6-7C7DFF26940B}.Debug|Any CPU.ActiveCfg = Debug|x86
+		{C326D55B-0411-46F5-B9A6-7C7DFF26940B}.Debug|Mixed Platforms.ActiveCfg = Debug|x86
+		{C326D55B-0411-46F5-B9A6-7C7DFF26940B}.Debug|Mixed Platforms.Build.0 = Debug|x86
+		{C326D55B-0411-46F5-B9A6-7C7DFF26940B}.Debug|x86.ActiveCfg = Debug|x86
+		{C326D55B-0411-46F5-B9A6-7C7DFF26940B}.Debug|x86.Build.0 = Debug|x86
+		{C326D55B-0411-46F5-B9A6-7C7DFF26940B}.Release|Any CPU.ActiveCfg = Release|x86
+		{C326D55B-0411-46F5-B9A6-7C7DFF26940B}.Release|Mixed Platforms.ActiveCfg = Release|x86
+		{C326D55B-0411-46F5-B9A6-7C7DFF26940B}.Release|Mixed Platforms.Build.0 = Release|x86
+		{C326D55B-0411-46F5-B9A6-7C7DFF26940B}.Release|x86.ActiveCfg = Release|x86
+		{C326D55B-0411-46F5-B9A6-7C7DFF26940B}.Release|x86.Build.0 = Release|x86
+		{3B5FBBEF-CC0C-41C9-AD5F-8037EBCC9A92}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{3B5FBBEF-CC0C-41C9-AD5F-8037EBCC9A92}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{3B5FBBEF-CC0C-41C9-AD5F-8037EBCC9A92}.Debug|Mixed Platforms.ActiveCfg = Debug|Any CPU
+		{3B5FBBEF-CC0C-41C9-AD5F-8037EBCC9A92}.Debug|Mixed Platforms.Build.0 = Debug|Any CPU
+		{3B5FBBEF-CC0C-41C9-AD5F-8037EBCC9A92}.Debug|x86.ActiveCfg = Debug|Any CPU
+		{3B5FBBEF-CC0C-41C9-AD5F-8037EBCC9A92}.Release|Any CPU.ActiveCfg = Debug|Any CPU
+		{3B5FBBEF-CC0C-41C9-AD5F-8037EBCC9A92}.Release|Any CPU.Build.0 = Debug|Any CPU
+		{3B5FBBEF-CC0C-41C9-AD5F-8037EBCC9A92}.Release|Mixed Platforms.ActiveCfg = Debug|Any CPU
+		{3B5FBBEF-CC0C-41C9-AD5F-8037EBCC9A92}.Release|Mixed Platforms.Build.0 = Debug|Any CPU
+		{3B5FBBEF-CC0C-41C9-AD5F-8037EBCC9A92}.Release|x86.ActiveCfg = Debug|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+EndGlobal
